397bcae5f702e5b4510ccf104d192e6d7253a16d
buffallos <p270640624@gmail.com>
2021-01-28 16:55:01 +0800

    New files

---
+++ WMI/Get-ServerInfo.ps1
@@ -0,0 +1,10 @@
+Function Get-ServerInfo {
+    [CmdletBinding()]
+    Param(
+        [Parameter(valuefrompipelinebypropertynam=$true)]
+        [string]$ComputerName=$env:COMPUTERNAME
+    )
+    Process {
+        
+    }
+}
--- test.ps1
+++ test.ps1
@@ -1,1 +1,12 @@
-﻿
+﻿Function Get-MLogicaldisk {
+    [CmdletBinding()]
+    Param(
+        [parameter()]
+        [string]$ComputerName=$env:COMPUTERNAME
+    )
+    Process {
+        $sess=New-CimSessionOption -Protocol Wsman
+        $cimsess=new-cimsession -ComputerName $ComputerName -SessionOption $sess
+        Get-CimInstance -ClassName win32_logicaldisk -CimSession $cimsess 
+    }    
+}
